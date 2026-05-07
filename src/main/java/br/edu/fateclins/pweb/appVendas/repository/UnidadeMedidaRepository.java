6b132fd4ee7bb306aeb87c39371accaca719a2d9
Igor Basquerotto <basquerotto@gmail.com>
2026-05-06 21:31:37 -0300

    aula06maio

--- src/main/java/br/edu/fateclins/pweb/appVendas/repository/UnidadeMedidaRepository.java
+++ src/main/java/br/edu/fateclins/pweb/appVendas/repository/UnidadeMedidaRepository.java
@@ -2,6 +2,9 @@ package br.edu.fateclins.pweb.appVendas.repository;
 
 import br.edu.fateclins.pweb.appVendas.modelo.UnidadeMedida;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UnidadeMedidaRepository extends JpaRepository<UnidadeMedida, Integer> {
 }
